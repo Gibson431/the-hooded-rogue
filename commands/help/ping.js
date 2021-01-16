8c169c2f7cd355959588ee84e165b04b88464262
Xekso <timmaxgibson@gmail.com>
2021-01-16 16:01:53 +1100

    testing the conversion to github

--- commands/help/ping.js
+++ commands/help/ping.js
@@ -5,6 +5,7 @@ module.exports = {
     minArgs: 0,
     maxArgs: 0,
     callback: ({message, args, text, client, prefix, instance}) => {
-        message.reply(`my status is currently set to: ${String(client.presence.status)}`)
+        // message.reply(`my status is currently set to: ${String(client.presence.status)}`)
+        message.reply('pong')
     }
 }
