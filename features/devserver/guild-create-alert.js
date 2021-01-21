692ff82629ae9c06a905fa5f199483d071447870
Xekso <timmaxgibson@gmail.com>
2021-01-22 02:06:26 +1100

    Added alert when joining new guild in the dev server

--- features/devserver/guild-create-alert.js
+++ features/devserver/guild-create-alert.js
@@ -2,7 +2,7 @@ const Embed = require("@configurations/embed-construct")
 
 module.exports = (client) => {
     client.on('guildCreate', async (newGuild) => {
-        const systemChannel = await client.channels.cache.get('782868510979784724') // TODO: put this in .env
+        const systemChannel = await client.channels.cache.get(process.env.SYSTEM_ID)
         systemChannel.send(Embed.guildCreate(newGuild))
     })
 }
