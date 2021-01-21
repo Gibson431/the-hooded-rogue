ef9f7bfc6348f957275e5f0eb50da84231401f97
Xekso <timmaxgibson@gmail.com>
2021-01-22 02:03:32 +1100

    added security

--- commands/dev/simerror.js
+++ commands/dev/simerror.js
@@ -2,6 +2,7 @@ module.exports = {
     commands: ['simerror'],
     category: 'Dev Commands',
     description: 'Simulates an error',
+    ownerOnly: true,
     callback: ({message, args, text, client}) => {
         message.reply(`Testing error...`)
         process.emitWarning('UnhandledPromiseRejectionWarning', `Test error`)
