4c497dc3a106238dd39650c777f8b2b4ed830cad
Xekso <timmaxgibson@gmail.com>
2021-01-22 02:09:48 +1100

    Added to dev help menu

--- commands/dev/simguildjoin.js
+++ commands/dev/simguildjoin.js
@@ -1,6 +1,8 @@
 module.exports = {
     commands: ['sgj'],
     ownerOnly: true,
+    category: 'Dev Commands',
+    description: 'Simulates the bot being added to a server.',
     callback: ({ message, client }) => {
         const { guild } = message
         message.reply(`Simulating adding the bot to a new server...`)
