be4040868bfe2a61a7cf2ede4f9df3bf28f2d1aa
Xekso <timmaxgibson@gmail.com>
2021-02-19 21:51:10 +1100

    fiddling

--- features/devserver/guild-create-alert.js
+++ features/devserver/guild-create-alert.js
@@ -3,7 +3,7 @@ const Embed = require("@configurations/embed-construct")
 module.exports = (client) => {
     client.on('guildCreate', async (newGuild) => {
         const systemChannel = await client.channels.cache.get(process.env.SYSTEM_ID)
-        systemChannel.send(Embed.guildCreate(newGuild))
+        systemChannel.send(await Embed.guildCreate(newGuild))
     })
 }
 
